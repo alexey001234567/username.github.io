18757a07264c46f15ce53d7f6871ed0d50fa10b6
alexey001234567 <64755311+alexey001234567@users.noreply.github.com>
2020-06-07 21:23:28 +0500

    Add files via upload

--- service worker.js
+++ service worker.js
@@ -12,3 +12,35 @@ self.addEventListener('fetch', (event) => {
 
 
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/img/',
+		'/img',
+      ]);
+    })
+  );
+});
+
+// self.addEventListener('fetch', function(event) {
+  // event.respondWith(caches.match(event.request).then(function(response) {
+ 
+    // if (response !== undefined) {
+      // return response;
+    // } else {
+      // return fetch(event.request).then(function (response) {
+ 
+        // let responseClone = response.clone();
+
+        // caches.open('v1').then(function (cache) {
+          // cache.put(event.request, responseClone);
+        // });
+        // return response;
+      // }).catch(function (e) {
+        // return caches.match('/');
+      // });
+    // }
+  // }));
+// });
