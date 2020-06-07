a45c5633ce4e652f73c296ad4a502cd61fd6e32e
alexey001234567 <64755311+alexey001234567@users.noreply.github.com>
2020-06-07 21:31:14 +0500

    Add files via upload

--- service worker.js
+++ service worker.js
@@ -1,28 +1,28 @@
-self.addEventListener('install', (event) => {
-    console.log('Установлен');
-});
+// self.addEventListener('install', (event) => {
+    // console.log('Установлен');
+// });
 
-self.addEventListener('activate', (event) => {
-    console.log('Активирован');
-});
+// self.addEventListener('activate', (event) => {
+    // console.log('Активирован');
+// });
 
-self.addEventListener('fetch', (event) => {
-    console.log('Происходит запрос на сервер');
-});
+// self.addEventListener('fetch', (event) => {
+    // console.log('Происходит запрос на сервер');
+// });
 
 
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/img/',
-		'/img',
-      ]);
-    })
-  );
-});
+// self.addEventListener('install', function(event) {
+  // event.waitUntil(
+    // caches.open('v1').then(function(cache) {
+      // return cache.addAll([
+        // '/',
+        // '/img/',
+		// '/img',
+      // ]);
+    // })
+  // );
+// });
 
 // self.addEventListener('fetch', function(event) {
   // event.respondWith(caches.match(event.request).then(function(response) {
